e8059020090a7c178be66a351360648859761709
StanislavPodolsky <mark_castle@mail.ru>
2014-12-28 18:09:47 +0200

    Fixed shared library linking

--- src/frontend/httmap/mainwindow.cpp
+++ src/frontend/httmap/mainwindow.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "nmap.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    char *res = read_ip_pull("/home/stanislav/Projects/httpmap/scan.txt");
     ui->setupUi(this);
 }
 
