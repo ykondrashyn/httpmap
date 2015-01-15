a4efca84f233dc6b5fb93e6fe00aceec39edbdd2
EvgeniyKondrashyn <e.kondrashyn@gmail.com>
2015-01-15 21:32:07 +0200

    added cheking ips in loop

--- tools/dcbip/dcbip.c
+++ tools/dcbip/dcbip.c
@@ -5,9 +5,10 @@
 #include <jansson.h>
 #include <curl/curl.h>
 
-#define BUFFER_SIZE (32 * 1024) /* 32 KB */
-#define URL_FORMAT   "http://rest.db.ripe.net/search.json?query-string=%s" /* ripe-db in json format */
-#define URL_SIZE     256
+#define BUFFER_SIZE (64 * 1024)  /* 32 KB */
+#define URL_FORMAT                  "http://rest.db.ripe.net/search.json?query-string=%s" /* ripe-db in json format */
+#define URL_SIZE                    256
+#define MAX_DIAPASONE_LEN           20 
 
 typedef int bool;
 enum { 
@@ -18,6 +19,33 @@ struct write_result {
     char *data;
     int pos;
 };
+
+char *read_ip_pull(const char *file) {
+    int size = 0;
+    int offset = 0;
+    char *result = NULL;
+    FILE *fd = fopen(file, "r");
+    if (fd) {
+        /* getting size of file */
+        fseek(fd, 0, SEEK_END);
+        size = ftell(fd);
+        if (!size)
+            return NULL;
+        fseek(fd, 0, SEEK_SET);
+        /* allocating the resources */
+        result = (char *)malloc(size);
+        if (result) {
+            while (fgets(result + offset, MAX_DIAPASONE_LEN, fd)) {
+                offset = strlen(result);
+            }
+        }
+        fclose(fd);
+        return result;
+    } else {
+        fprintf(stderr, "An error has been occurred while opening the '%s'\n", file);
+        return NULL;
+    }
+}
 
 static size_t write_response(void *ptr, size_t size, size_t nmemb, void *stream) {
     struct write_result *result = (struct write_result *)stream;
@@ -108,89 +136,98 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
-    strval = NULL;
-    found = false;
+    char *pch = NULL;
+    char *strings = read_ip_pull(argv[1]);
+    pch = strtok(strings,"\n");
 
-    snprintf(url, URL_SIZE, URL_FORMAT, argv[1]);
+    while (pch) {
+        printf("Checking ip diapasone: %s\n", pch);
 
-    text = request(url);
-    if(!text)
-        return 1;
+        strval = NULL;
+        found = false;
 
-    root = json_loads(text, 0, &error);
-    free(text);
+        snprintf(url, URL_SIZE, URL_FORMAT, pch);
 
-    if(!root) {
-        fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
-        return 1;
-    }
+        text = request(url);
+        if(!text)
+            return 1;
 
-    if(!json_is_object(root)) {
-        fprintf(stderr, "error: root is not an object\n");
-        json_decref(root);
-        return 1;
-    }
+        root = json_loads(text, 0, &error);
+        free(text);
 
-     objects_ = json_object_get(root, "objects");
-     if(!json_is_object(objects_))
-     {
-         fprintf(stderr, "is not an object\n", (int)(i + 1));
-         json_decref(objects_);
-         return 1;
-     }
+        if(!root) {
+            fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+            return 1;
+        }
 
-    object_arr = json_object_get(objects_, "object");
-    if(!json_is_array(object_arr)) {
-        fprintf(stderr, "error: object_arr is not an array\n");
-        json_decref(object_arr);
-        return 1;
-    }
+        if(!json_is_object(root)) {
+            fprintf(stderr, "error: root is not an object\n");
+            json_decref(root);
+            return 1;
+        }
 
-    for(i = 0; i < json_array_size(object_arr); i++) {
-        if(found == true)
-            break;
-        data = json_array_get(object_arr, i);
-        if(!json_is_object(data))
-            fprintf(stderr, "error: data %d is not an object\n", (int)(i + 1));
-        attributes_ = json_object_get(data, "attributes");
-        if(!json_is_object(attributes_)) {
+        objects_ = json_object_get(root, "objects");
+        if(!json_is_object(objects_))
+        {
             fprintf(stderr, "is not an object\n", (int)(i + 1));
-            json_decref(attributes_);
+            json_decref(objects_);
             return 1;
         }
 
-        attributes_arr = json_object_get(attributes_, "attribute");
-        if(!json_is_array(attributes_arr)) {
-            fprintf(stderr, "error: attributes_arr is not an array\n");
-            json_decref(attributes_arr);
+        object_arr = json_object_get(objects_, "object");
+        if(!json_is_array(object_arr)) {
+            fprintf(stderr, "error: object_arr is not an array\n");
+            json_decref(object_arr);
             return 1;
         }
 
-        for(j = 0; j < json_array_size(attributes_arr); j++) {
-            target = json_array_get(attributes_arr, j);
-            if(!json_is_object(target))
-                fprintf(stderr, "error: data %d is not an object\n", (int)(i + 1));
-            value = json_object_get(target, "value");
-            strval = strstr(json_string_value(value), argv[2]);
-            if(strval) {
-                found = true;
+        for(i = 0; i < json_array_size(object_arr); i++) {
+            if(found == true)
                 break;
+            data = json_array_get(object_arr, i);
+            if(!json_is_object(data))
+                fprintf(stderr, "error: data %d is not an object\n", (int)(i + 1));
+            attributes_ = json_object_get(data, "attributes");
+            if(!json_is_object(attributes_)) {
+                fprintf(stderr, "is not an object\n", (int)(i + 1));
+                json_decref(attributes_);
+                return 1;
+            }
+
+            attributes_arr = json_object_get(attributes_, "attribute");
+            if(!json_is_array(attributes_arr)) {
+                fprintf(stderr, "error: attributes_arr is not an array\n");
+                json_decref(attributes_arr);
+                return 1;
+            }
+
+            for(j = 0; j < json_array_size(attributes_arr); j++) {
+                target = json_array_get(attributes_arr, j);
+                if(!json_is_object(target))
+                    fprintf(stderr, "error: data %d is not an object\n", (int)(i + 1));
+                value = json_object_get(target, "value");
+                strval = strstr(json_string_value(value), argv[2]);
+                if(strval) {
+                    found = true;
+                    break;
+                }
             }
         }
-    }
 
-    if(strval) {
-        printf("Found value '%s' in %s\nATTR: %s\n", argv[2], argv[1], strval);
-        fd = fopen("log.txt","w");
-        if(fd != NULL) {
-            fputs (argv[1], fd);
-            fputc ('\n', fd);
-            fclose (fd);
+        if(strval) {
+            printf("Found value '%s' in %s\nATTR: %s\n", argv[2], pch, strval);
+            fd = fopen("log.txt","a+");
+            if(fd != NULL) {
+                fputs (pch, fd);
+                fputc ('\n', fd);
+                fclose (fd);
+            }
+            else
+                fprintf(stderr, "Error opening file");
+
         }
-        else
-            fprintf(stderr, "Error opening file");
-
+        json_decref(root);
+        pch = strtok(NULL, "\n");
     }
-    json_decref(root);
     return 0;
 }
